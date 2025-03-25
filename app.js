8a191e119dedfe52295e99cc293625b6641edf6d
Mariacmv <mariaclaragmviana@gmail.com>
2025-03-25 19:12:25 -0300

    Finalizando boas práticas de programação

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 alert(`Boas vindas ao jogo do número secreto!`);
-let numeroSecreto = parseInt(Math.random() * 100 + 1);//funcionalidade math.random. Soma 1 porque não inclui o último valor
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);//funcionalidade math.random. Soma 1 porque não inclui o último valor
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1; //tem que começar com 1
 
-while (chute != numeroSecreto){ //enquanto não for igual ao número secreto
-    chute = prompt(`Escolha um número entre 1 e 100: `);
+while (chute != numeroSecreto) { //enquanto não for igual ao número secreto
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}: `);
 
-    if (chute == numeroSecreto){
+    if (chute == numeroSecreto) {
         break;
-    } else{
-        if (chute > numeroSecreto){
+    } else {
+        if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
-        }else{
+        } else {
             alert(`O número secreto é maior que ${chute}`);
         }
-        tentativas ++;
+        tentativas++;
     }
 }
 
 let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa' //operador ternário
 alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
 
-if (tentativas > 1){
+if (tentativas > 1) {
     alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
-} else{
+} else {
     alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
 }
