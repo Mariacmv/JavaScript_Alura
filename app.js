0b65b194992193d90dc9639a90c97730aca9c1f9
Mariacmv <mariaclaragmviana@gmail.com>
2025-04-15 12:26:35 -0300

    Resolvendo problema de merge

--- app.js
+++ app.js
@@ -24,29 +24,6 @@
 // let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
 // alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
 
-alert(`Boas vindas ao jogo do número secreto!`);
-let numeroMaximo = 5000;
-let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);//funcionalidade math.random. Soma 1 porque não inclui o último valor
-console.log(numeroSecreto);
-let chute;
-let tentativas = 1; //tem que começar com 1
-
-while (chute != numeroSecreto) { //enquanto não for igual ao número secreto
-    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}: `);
-
-    if (chute == numeroSecreto) {
-        break;
-    } else {
-        if (chute > numeroSecreto) {
-            alert(`O número secreto é menor que ${chute}`);
-        } else {
-            alert(`O número secreto é maior que ${chute}`);
-        }
-        tentativas++;
-    }
-}
-
-
 // // if (tentativas > 1) {
 // //     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
 // // } else {
