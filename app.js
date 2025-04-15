8bb1cf484410a1033799b7bf83aa596591502b2b
Mariacmv <mariaclaragmviana@gmail.com>
2025-04-15 12:21:33 -0300

    Commitando para salvar

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
-alert(`Boas vindas ao jogo do número secreto!`);
-let numeroSecreto = parseInt(Math.random() * 100 + 1);//funcionalidade math.random. Soma 1 porque não inclui o último valor
-console.log(numeroSecreto);
-let chute;
-let tentativas = 1; //tem que começar com 1
+// alert('Boas vindas ao jogo do número secreto');
+// let numeroSecreto = parseInt(Math.random() * 100 + 1);
+// console.log(numeroSecreto);
+// let chute;
+// let tentativas = 1;
 
-while (chute != numeroSecreto){ //enquanto não for igual ao número secreto
-    chute = prompt(`Escolha um número entre 1 e 100: `);
+// // enquanto chute não for igual ao n.s.
+// while (chute != numeroSecreto) {
+//     chute = prompt('Escolha um número entre 1 e 100');
+//     // se chute for igual ao número secreto
+//     if (chute == numeroSecreto) {
+//         break;
+//     } else {
+//         if (chute > numeroSecreto) {
+//             alert(`O número secreto é menor que ${chute}`);
+//         } else {
+//             alert(`O número secreto é maior que ${chute}`);
+//         }
+//         // tentativas = tentativas + 1;
+//         tentativas++;
+//     }
+// }
 
-    if (chute == numeroSecreto){
-        break;
-    } else{
-        if (chute > numeroSecreto){
-            alert(`O número secreto é menor que ${chute}`);
-        }else{
-            alert(`O número secreto é maior que ${chute}`);
-        }
-        tentativas ++;
-    }
-}
+// let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+// alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
 
-let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa' //operador ternário
-alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+// // if (tentativas > 1) {
+// //     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+// // } else {
+// //     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+// // }
 
-if (tentativas > 1){
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
-} else{
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
-}
+
+
+//Para manipular uma tag do html:
+// Cria uma variável para receber e só modifica o 1 h1 por causa do let
+let titulo = document.querySelector('h1');
+// Agora modifica o html
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
